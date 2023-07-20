aad56a43103c5de211f764718b020e32e78c497d
sawonhossain34 <sawonhossain3491@gmail.com>
2023-07-20 19:49:22 +0600

    home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,25 @@
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            {/* ////////   hero section  ////// */}
+            <section className="hero__section pt-[60px] 2xl:h-[800px]">
+                <div className="container">
+                    <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+
+                        {/* //////  hero content  ////// */}
+                        <div>
+                            <div className="lg:w-[570px]">
+                                <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:Leading-[70px]"> 
+                                    We help patients live a healthy life.
+                                </h1>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
